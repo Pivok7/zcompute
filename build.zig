34977a1f69babb8e25c05e14303a16ae57970488
Pivok <pivoc@protonmail.com>
2025-08-26 10:56:28 +0200

    Update build

--- build.zig
+++ build.zig
@@ -8,14 +8,14 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/zcompute.zig"),
         .target = target,
         .optimize = optimize,
+        .link_libc = true,
     });
 
     // Libraries
-    const vulkan_zig_deb = b.dependency("vulkan", .{
+    const vulkan_zig_dep = b.dependency("vulkan", .{
         .registry = b.dependency("vulkan_headers", .{}).path("registry/vk.xml"),
-    }).module("vulkan-zig");
-    lib.addImport("vulkan", vulkan_zig_deb);
-
-    lib.linkSystemLibrary("vulkan", .{});
-    lib.link_libc = true;
+        .target = target,
+        .optimize = optimize,
+    });
+    lib.addImport("vulkan", vulkan_zig_dep.module("vulkan-zig"));
 }
